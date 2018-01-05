c3c69aef540dc30e46ac13f24a7393ef5f922920
Eman Herawy <emisky_vip@yahoo.com>
2018-01-05 16:40:32 +0200

    ng drawing board init with basic features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Draw Board App';
+
+  ngOnInit(): void { }
+
 }
